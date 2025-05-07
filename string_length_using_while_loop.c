6f52a3fb4a33cf1b471872f404a85c2b9c3da2ed
Ashish Varanasi <ashishvaranasi77@gmail.com>
2025-05-07 23:32:40 +0530

    added string length program

---
+++ string_length_using_while_loop.c
@@ -0,0 +1,27 @@
+//Program for finding length of a string with while loop
+
+#include <stdio.h>
+
+int len(char name[100])
+{
+    int i=0;
+    while (i>=0)
+    {
+        if (name[i] == '\0')
+        {
+            break;
+        }
+        i++;
+    }
+    return i;
+}
+
+    
+int main() {
+    char name[100];
+    printf("Enter your name: ");
+    scanf("%s", name);
+    int result=len(name); 
+    printf("Length of string: %d\n", result);
+    return 0;
+}
